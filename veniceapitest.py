6dd2a3448a3d283ae6f035037bec134ac429e587
k9music729-lab <k9music729@gmail.com>
2025-07-24 13:52:33 +0530

    asd
    asd

---
+++ veniceapitest.py
@@ -0,0 +1,38 @@
+import requests
+import json
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key=os.getenv('api_key')
+
+url = "https://openrouter.ai/api/v1/chat/completions"
+
+headers = {
+    "Authorization": api_key,  # ✅ Add 'Bearer ' prefix
+    "Content-Type": "application/json",
+    "HTTP-Referer": "https://your-site.com",  # Optional
+    "X-Title": "TestApp"                     # Optional
+}
+
+story=st.text_input('Enter some text')
+
+payload = {
+    "model": "mistralai/mistral-small-3.2-24b-instruct",
+    "messages": [
+        {"role": "user", "content":f"{story}"}
+    ]
+}
+
+if st.button('Generate Response'):
+    if story:
+        response = requests.post(url, headers=headers, data=json.dumps(payload))
+        with st.spinner("Loading Response"):
+            if response.status_code==200:
+                print("Status:", response.status_code)
+                print("Response:")
+                print(response.json()["choices"][0]["message"]["content"])
+                st.write(response.json()["choices"][0]["message"]["content"])
+
